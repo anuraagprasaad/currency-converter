b0a1290ef2e4c20926fb74742cd2ddbb49f90e85
anuraagprasaad <anuraagprasaad@gmail.com>
2017-02-05 16:16:16 +0800

    Update Application.java

--- src/main/java/com/currency/conversion/Application.java
+++ src/main/java/com/currency/conversion/Application.java
@@ -1,11 +1,7 @@
 package com.currency.conversion;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
@@ -18,19 +14,7 @@ import org.springframework.web.context.request.RequestContextListener;
 @SpringBootApplication
 @Import({ ApplicationConfig.class, SecurityConfig.class, WebConfig.class })
 public class Application extends SpringBootServletInitializer {
-  
-	@Value("${spring.datasource.url}")
-	private String dbUrl;
-	
-	@Value("${spring.datasource.username}")
-	private String username;
-	
-	@Value("${spring.datasource.driver-class-name}")
-	private String driverClassName;
-	
-	@Value("${spring.datasource.password}")
-	private String password;
-	
+  		
 	@Bean
 	public RequestContextListener requestContextListener() {
 		return new RequestContextListener();
@@ -49,15 +33,6 @@ public class Application extends SpringBootServletInitializer {
 	@Primary
 	public Validator localValidatorFactoryBean() {
 	    return new LocalValidatorFactoryBean();
-	}
+	}	
 	
-	@Bean
-	public DataSource dataSource() {
-	    DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-	        dataSourceBuilder.url(dbUrl);
-	        dataSourceBuilder.driverClassName(driverClassName);
-	        dataSourceBuilder.username(username);
-	        dataSourceBuilder.password(password);
-	        return dataSourceBuilder.build();   
-	}
 }
