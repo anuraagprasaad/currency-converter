d5b0820914034d8bb250d4c908266c849d8b7e93
anuraagprasaad <anuraagprasaad@gmail.com>
2017-02-06 12:52:29 +0800

    Update TestConfig.java

--- src/test/java/com/currency/conversion/test/cucumber/TestConfig.java
+++ src/test/java/com/currency/conversion/test/cucumber/TestConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
@@ -18,36 +17,22 @@ import com.currency.conversion.SecurityConfig;
 @Configuration
 @EnableAutoConfiguration
 @Import({ ApplicationConfig.class, SecurityConfig.class })
-@ActiveProfiles("test")
 @Component
-/*@EnableJpaRepositories(basePackages = "com.currency.conversion.dao")
-@ComponentScan(basePackages = { "com.currency.conversion.service", "com.currency.conversion.dao", "com.currency.conversion.model" })*/
 public class TestConfig {
-	
-/*	@Value("${spring.datasource.url}")
-	private String dbUrl;
-	
-	@Value("${spring.datasource.username}")
-	private String username;
-	
-	@Value("${spring.datasource.driver-class-name}")
-	private String driverClassName;
-	
-	@Value("${spring.datasource.password}")
-	private String password;*/
-	
+
 	@Bean
 	public Validator localValidatorFactoryBean() {
 		return new LocalValidatorFactoryBean();
 	}
-	
+
 	@Bean
 	public DataSource dataSource() {
-	    DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-	        dataSourceBuilder.url("jdbc:mysql://sql6.freemysqlhosting.net:3306/sql6154997?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull");
-	        dataSourceBuilder.driverClassName("com.mysql.jdbc.Driver");
-	        dataSourceBuilder.username("sql6154997");
-	        dataSourceBuilder.password("pvqUKLzxAT");
-	        return dataSourceBuilder.build();   
+		DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+		dataSourceBuilder.url(
+				"jdbc:mysql://sql6.freemysqlhosting.net:3306/sql6154997?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull");
+		dataSourceBuilder.driverClassName("com.mysql.jdbc.Driver");
+		dataSourceBuilder.username("sql6154997");
+		dataSourceBuilder.password("pvqUKLzxAT");
+		return dataSourceBuilder.build();
 	}
 }
