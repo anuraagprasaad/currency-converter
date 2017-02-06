f8e71fcfa2ce802c0b91a6266ed4feb5b2000007
anuraagprasaad <anuraagprasaad@gmail.com>
2017-02-06 12:45:54 +0800

    Update ApplicationConfig.java

--- src/main/java/com/currency/conversion/ApplicationConfig.java
+++ src/main/java/com/currency/conversion/ApplicationConfig.java
@@ -6,7 +6,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.context.request.RequestContextListener;
 
@@ -14,13 +13,12 @@ import org.springframework.web.context.request.RequestContextListener;
 @Configuration
 @EnableCaching
 @EnableJpaRepositories(basePackages = "com.currency.conversion.dao")
-@ComponentScan(basePackages = { "com.currency.conversion.service", "com.currency.conversion.dao", "com.currency.conversion.model" })
+@ComponentScan(basePackages = { "com.currency.conversion.service", "com.currency.conversion.dao",
+		"com.currency.conversion.model" })
 @EntityScan("com.currency.conversion.model")
-@Profile("development")
 public class ApplicationConfig {
 	@Bean
 	public RequestContextListener requestContextListener() {
 		return new RequestContextListener();
 	}
-		
-	}
+}
