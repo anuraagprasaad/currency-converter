617b746a0c2ca8e36379a3f4f1ca6ed28be3abe0
anuraagprasaad <anuraagprasaad@gmail.com>
2017-02-06 12:46:54 +0800

    Update WebConfig.java

--- src/main/java/com/currency/conversion/WebConfig.java
+++ src/main/java/com/currency/conversion/WebConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-//@EnableWebMvc
 @ComponentScan("com.currency.conversion.web")
 public class WebConfig extends WebMvcConfigurerAdapter {
 }
